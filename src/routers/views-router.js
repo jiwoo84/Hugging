cd0822fa75019f4122c8d96df4b80de62082c193
Jiwoo <wldn0804@gmail.com>
2022-11-02 11:26:13 +0900

    Design: change header&footer css

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -11,6 +11,7 @@ viewsRouter.use("/register", serveStatic("register"));
 viewsRouter.use("/login", serveStatic("login"));
 viewsRouter.use("/cart", serveStatic("cart"));
 viewsRouter.use("/admin", serveStatic("admin"));
+viewsRouter.use("/mypage", serveStatic("mypage"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));

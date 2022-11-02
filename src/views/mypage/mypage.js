c683b1411b5b9fb5a978408c75332588b841ba4d
strombreeding <speaker1403@gmail.com>
2022-11-02 12:16:10 +0900

    feat: add API-mypage

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -3,6 +3,6 @@ import * as Api from "/api.js";
 window.addEventListener("load", getName);
 
 async function getName() {
-    const user = await Api.get('/api/users/mypage');
-    const { user } = 
+  const user = await Api.get("/api/users/mypage");
+  // 지우님 user 콘솔한번 찍어보시고 어떻게 데이터가 왔는지 확인해보세요!
 }

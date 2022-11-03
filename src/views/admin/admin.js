4414479f683de0f61b9cb82817b1b3f3f37e71aa
Jiwoo <wldn0804@gmail.com>
2022-11-03 23:12:48 +0900

    feat: 상품조회 출력

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -2,9 +2,8 @@ import * as Api from "/api.js";
 
 const bigDiv = document.querySelector("#list-box");
 const orderBtn = document.querySelector("#orderBtn");
-const itemBtn = document.querySelector("#itemBtn");
+const itemBtn = document.querySelector("#itemsBtn");
 const categoryBtn = document.querySelector("#categoryBtn");
-
 orderBtn.addEventListener("click", clickedOrder);
 itemBtn.addEventListener("click", clickedItem);
 categoryBtn.addEventListener("click", clickedCategory);
@@ -77,7 +76,7 @@ async function clickedOrder() {
 
     if (data.data[i].수정 === "수정가능") {
       thirdDiv.innerHTML = `
-        <button id="${data.data[i].주문번호}" class="delBtn">주문삭제</button>
+        <button id="${data.data[i].주문번호} class="delBtn">주문삭제</button>
         <label for="${data.data[i].주문번호}">배송상태변경</label>
         <select id="${data.data[i].주문번호}" class="selectShippingState">
             <option>배송준비중</option>
@@ -131,27 +130,84 @@ async function clickedOrder() {
 }
 
 async function clickedItem() {
-  alert("아이템 추가/수정/삭제");
-  $.ajax({
-    type: "GET",
-    url: "/api/items/admin",
-    headers: { authorization: `Bearer ${sessionStorage.getItem("token")}` },
-    success: (res) => {
-      const addItem = document.getElementById("addItem");
-      addItem.innerHTML = "";
-      console.log(res.data);
-      for (let i = 0; i < res.data.length; i++) {
-        console.log("포문");
-        const div = document.createElement("div");
-        div.id = res.data[i]._id;
-        div.innerHTML = `
-            <p>상품이름 : ${res.data[i].name}</p>
-            <p>카테고리 : ${res.data[i].category}</p>
-            <img src="${res.data[i].imageUrl}"/>
-            <p>가격 : ${res.data[i].price}원</p>
-            `;
-        addItem.appendChild(div);
-      }
-    },
+  // 화면 초기화
+  bigDiv.innerHTML = `
+  <div id="productsListBox">
+    <table>
+      <thead>
+        <tr>
+          <th>이름</th>
+          <th>카테고리</th>
+          <th>가격</th>
+          <th>이미지</th>
+          <th>생성날짜</th>
+          <th>현재판매량</th>
+          <th>상세내용</th>
+        </tr>
+      </thead>
+      <tbody id="tableBody">
+      </tbody>
+    </table>
+  </div>
+  <div id="productDetailBox"></div>
+  `;
+
+  const data = await Api.get("/api/items/admin");
+  // 리스트가 들어갈 표의 body
+  const tableBody = document.querySelector("#tableBody");
+
+  for (let i = 0; i < data.data.length; i++) {
+    const productObj = data.data[i];
+    console.log(productObj);
+    // 한 행 생성
+    const tr = document.createElement("tr");
+    tr.id = productObj._id;
+    // 행 안에 값 넣기
+    tr.innerHTML = `
+    <td>${productObj.name}</td>
+    <td>${productObj.category}</td>
+    <td>${productObj.price}</td>
+    <td>
+      <img src=${productObj.imageUrl} id="listImg"/>
+    </td>
+    <td>${productObj.createdAt.slice(0, 10)}</td>
+    <td>${productObj.sales}</td>
+    <td>${productObj.itemDetail}</td>
+    <td>
+      <button id=${productObj._id} class="productModifyBtn">상품수정</button>
+      <button id=${productObj._id} class="productDelBtn">상품삭제</button>
+    </td>
+    `;
+    tableBody.appendChild(tr);
+  }
+
+  // 상품 삭제
+  const productDelBtns = document.querySelectorAll(".productDelBtn");
+
+  productDelBtns.forEach((btn) => {
+    btn.addEventListener("click", async () => {
+      const id = btn.id;
+      const res = await Api.delete(`api/items/${id}`);
+      console.log(res.msg);
+      clickedItem();
+    });
+  });
+
+  // 상품 수정
+  const productModifyBtn = document.querySelectorAll(".productModifyBtn");
+  const productDetailBox = document.querySelector("#productDetailBox");
+
+  productModifyBtn.forEach((btn) => {
+    btn.addEventListener("click", async () => {
+      // 아래에 페이지 추가로 생성
+      const id = btn.id;
+      const productInfo = await Api.get(`api/items`, id);
+      console.log(productInfo);
+      // productDetailBox.innerHTML = `
+      //   <p></p>
+      // `
+      // 거기에 레이아웃 만들어서 데이터 + 수정버튼 뿌림
+      // 수정 버튼 누르면 input 만들어지게 함
+    });
   });
 }

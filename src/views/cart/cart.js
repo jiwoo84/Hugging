ac3c7b5a69a7ee419135f6844aafc09e92ab4a9e
Park Ji Hye <qkrwlgp235@gmail.com>
2022-11-03 12:01:45 +0900

    충돌처리

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,44 +1,110 @@
-const modal = `
-<div class="modal-card" style="border:black solid 1px">
-    <img src="../elice-rabbit.png" alt="item"/>
-    <h3 class="item-name">sofa</h3>
-    <button class="plus">+</button>
-    <label class="quantity">1</label>
-    <button class="minus">-</button>
-    <p class="item-price">1000</p>
-</div>
-`;
+const main = document.querySelector(".main");
+const init = document.querySelector(".init-msg");
+const clearbtns = document.querySelector(".clear-btn-container");
+const clearAllBtn = document.querySelector(".clear-all");
+const clearSelectBtn = document.querySelector(".clear-select");
+const checkbox = document.querySelector("#check");
 
-if (window.indexedDB) {
-    const databaseName = 'cart';
-    const version = 1;
+getIdxedDBValues();
 
-    const request = indexedDB.open(databaseName, version);
 
-    const data = {
-        id :1,
-        name: "sofa"
-    };
-
-    request.onupgradeneeded = function () {
-        // Object Store 생성
-        // 데이터베이스 아래 객체 스토어라는 이름으로 또다시 객체를 만들 수 있습니다.
-        request.result.createObjectStore('items', { autoIncrement: true });
-    };
-
-    request.onsuccess = function () {
-        const store = request.result
-            .transaction('item', 'readwrite')
-            .objectStore('items');
-            // transaction : 통신 , items 객체스토어의 권한을 readwrite로 설정
-            // objectStore : 오브젝트스토어를 가져옴
-
-        //데이터저장
-        store.add(data);
-    };
-
-    reader.onerror = function (error) {
-        alert('Error: ', error);
-        document.querySelector('body').removeChild(modalEl);
-    };
+function createPost(item,key) {
+    // const modalEl = document.createElement('input');
+    // modalEl.setAttribute("type","checkbox");
+    // modalEl.setAttribute("value", `${key}`);
+    return `
+    <div style="border 1px black">
+        <input type="checkbox" value="${key}" >
+        <p onclick="selected()">${item.name}</p>
+        <p>${item.category}</p>
+        <p>${item.price}</p>
+        <img src="${item.img}">
+        <p>${item.sales}</p>
+        <button class="plus">+</button>
+        <label class="quantity">1</label>
+        <button class="minus">-</button>
+        <label class="total-price">금액합계:${item.price}</label>
+    </div>
+    `;
 }
+
+
+// 전체데이터 조회
+function getIdxedDBValues() {
+    if (window.indexedDB) {
+        const request = indexedDB.open("cart");      // 1. DB 열기
+
+        request.onerror = (e)=> console.log(e.target.errorCode);
+        request.onsuccess = (e)=> {
+            const db = request.result;
+            const objStore = db.transaction("items").objectStore("items");  // 2. name 저장소 접근
+            const countRequest = objStore.count();
+            
+            countRequest.onsuccess = function() {
+                const recordCount = countRequest.result;
+                // console.log(recordCount); 
+                if(recordCount < 1){
+                    init.style.visibility = 'visible';
+                    clearbtns.style.visibility = 'hidden';
+                }
+                else{
+                    init.style.visibility = 'hidden';
+                    clearbtns.style.visibility = 'visible';
+
+                    const cursorRequest = objStore.openCursor();
+                    cursorRequest.onsuccess =(e)=> {
+                        let cursor = e.target.result;
+                        if (cursor) {
+                            const value = objStore.get(cursor.key);         // 3. 커서를 사용해 데이터 접근
+                            value.onsuccess =(e)=> {
+                                main.innerHTML += createPost(value.result,cursor.key);
+                            }
+                            cursor.continue();                              // 4. cursor로 순회
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+//전체삭제
+clearAllBtn.addEventListener("click",function(){
+    const request = window.indexedDB.open('cart');     // 1. db 열기
+    request.onerror =(e)=> console.log(e.target.errorCode);
+
+    request.onsuccess =(e)=> {
+        const transaction  = request.result.transaction('items', 'readwrite');
+        transaction.onerror =(e)=> console.log('fail');
+        transaction.oncomplete =(e)=> console.log('success');
+
+        const objStore = transaction.objectStore('items');   // 2. name 저장소 접근
+        const objStoreRequest = objStore.clear();           // 3. 전체 삭제
+        objStoreRequest.onsuccess =(e)=> {
+            console.log('cleared');
+        }
+    }
+})
+
+//선택삭제
+clearSelectBtn.addEventListener("click",function(){
+    const request = window.indexedDB.open('cart');     // 1. db 열기
+    request.onerror =(e)=> console.log(e.target.errorCode);
+
+    request.onsuccess =(e)=> {
+        const db = request.result;
+        const transaction = db.transaction('items', 'readwrite');
+        transaction.onerror =(e)=> console.log('fail');
+        transaction.oncomplete =(e)=> console.log('success');
+    
+        const objStore = transaction.objectStore('items');   // 2. name 저장소 접근
+        // const keys = getCheckboxValue();
+        // console.log(keys);
+        // keys.forEach((key)=>{
+        //     const objStoreRequest = objStore.delete(key);       // 3. 삭제하기 
+        //     objStoreRequest.onsuccess =(e)=> {
+        //         console.log('deleted');
+        //     }
+        // })
+    }
+})
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,16 +1,8 @@
-// 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
-// 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
-// 코드 예시를 남겨 두었습니다.
-
 import * as Api from "/api.js";
-import { randomId } from "/useful-functions.js";
+// import { randomId } from "/useful-functions.js";
 
-// 요소(element), input 혹은 상수
 const bestContainer= document.querySelector(".bestContainer");
 const newContainer = document.querySelector(".newContainer");
-
-const landingDiv = document.querySelector("#landingDiv");
-const greetingDiv = document.querySelector("#greetingDiv");
 
 getDataFromApi();
 
@@ -57,47 +49,57 @@ function alertGreetingText() {
 }
 
 async function getDataFromApi() {
-  // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
+
   const data = await Api.get("/api/items");
   // data = [ [{...},{...}...{...}:8개] , [{...}{}{}:3개] ]
   const {bestItems, newItems} = data;
 
-  for(let i =0; i<6;i++){
-    let card = document.createElement ('card')
-    let name = document.createElement ('p')
-    let img = document.createElement ('img')
-    let price = document.createElement ('p')
-    let category = document.createElement ('p')
-    
-    name.innerHTML = bestItems[i].name;
-    img.setAttribute('src', bestItems[i].imageUrl);
-    price.innerHTML = bestItems[i].price;
-    category.innerHTML = bestItems[i].category;
-    
-    card.appendChild(img);
-    card.appendChild(name);
-    card.appendChild(price);
-    card.appendChild(category);
-    bestContainer.appendChild(card);
-  }
-  //addeventlistener
-  for(let i =0; i<newItems.length;i++){
-    let card = document.createElement ('card')
-    let name = document.createElement ('p')
-    let img = document.createElement ('img')
-    let price = document.createElement ('p')
-    let category = document.createElement ('p')
-    
-    name.innerHTML = newItems[i].name;
-    img.setAttribute('src', newItems[i].imageUrl);
-    price.innerHTML = newItems[i].price;
-    category.innerHTML = newItems[i].category;
-    
-    card.appendChild(img);
-    card.appendChild(name);
-    card.appendChild(price);
-    card.appendChild(category);
-    newContainer.appendChild(card);
   
+  bestContainer.appendChild(draw(bestItems,"bestItem"));
+  attachBtn(className);
+  newContainer.appendChild(draw(newItems,"newItem"));
+}
+
+function draw(Items,className){
+  let card = document.createElement ('div')
+  for(let i =0; i<Items.length;i++){
+    card.innerHTML +=`
+    <div id="${Items[i]._id}" class="${className}">
+      <p>${Items[i].name}</p>
+      <img src="${Items[i].imageUrl}">
+      <p>${Items[i].price}</p>
+      <p>${Items[i].category}</p>
+    </div>
+    `;
+  }
+  return card;
+}
+
+function createDB(){
+  if (window.indexedDB) {
+    const databaseName = "cart";
+    const version = 1;
+
+    const request = indexedDB.open(databaseName, version);
+
+    request.onupgradeneeded = function () {
+      request.result.createObjectStore("items", { autoIncrement: true });
+    };
+    request.onsuccess = function () {};
+    request.onerror = function (event) { alert(event.target.errorCode);}
   }
 }
+
+function attachBtn(className){
+  const detailToBtns = document.querySelectorAll(`.${className}`);
+  console.log(detailToBtns);
+
+  detailToBtns.forEach( (detailToBtn)=>{
+    detailToBtn.addEventListener("click",()=>{
+      const id = detailToBtn.id;
+      localStorage.setItem('itemDetail',`${id}`);
+      location.href = "/detail";
+    })
+  });
+}
+

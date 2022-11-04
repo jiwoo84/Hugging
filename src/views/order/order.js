a548238ec5f3cac3f32edeef4ed50740c3c78556
Park Ji Hye <qkrwlgp235@gmail.com>
2022-11-04 16:28:43 +0900

    사용자getApi

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -1,11 +1,29 @@
 import * as Api from "/api.js";
 
+const orderedMan= document.querySelector(".orderedMan");
+const orderedAdress= document.querySelector(".orderedAdress");
+
 // 요소(element), input 혹은 상수
 getDataFromApi();
 
 async function getDataFromApi() {
     const user = await Api.get("/api/users","mypage");
+    const {name, address, phoneNumber} =user.data;
+    addUserComponent(name,address,phoneNumber);
 
-    console.log(user.data);
-    //이름, 전화번호, 주소
+}
+
+function addUserComponent(name,address,phoneNumber){
+    const nameInfo =  document.createElement("div");
+    const addressInfo = document.createElement("div");
+    
+    nameInfo.innerHTML = `<p>${name}</p>`;
+    addressInfo.innerHTML = `
+        <p>${name}</p>
+        <p>${address}</p>
+        <p>${phoneNumber}</p>
+    `;
+
+    orderedMan.appendChild(nameInfo);
+    orderedAdress.appendChild(addressInfo);
 }

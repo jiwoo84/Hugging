880d3d951fa6b02e21a0e31d6eb5e5d627cc5acb
strombreeding <speaker1403@gmail.com>
2022-11-05 18:22:27 +0900

    HOTFIX: orderList & categorySchema

--- src/db/schemas/category-schema.js
+++ src/db/schemas/category-schema.js
@@ -2,7 +2,7 @@ import { Schema } from "mongoose";
 
 const categorySchema = new Schema({
   name: { type: String, unique: true },
-  index: String,
+  index: { type: String, unique: true },
   items: [{ type: Schema.Types.ObjectId, ref: "Item" }],
 });
 

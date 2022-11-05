75009cb196d6c65477aa8c862a21de29d39c4b1f
strombreeding <speaker1403@gmail.com>
2022-11-05 13:43:04 +0900

    FEAT: add 마이페이지 주문목록 조회 & api 수정 & 주문목록 css

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -55,7 +55,6 @@ class OrderService {
       const orders = await Order.find({ buyer: data }) // 현재까지 주문한 모든 목록
         .populate("items.id")
         .populate("buyer");
-      console.log(orders);
       let result = [];
       for (let i = 0; i < orders.length; i++) {
         let obj = {}; // json형태로 반환하려고 만든것
@@ -69,6 +68,7 @@ class OrderService {
           });
         }
         obj = {
+          대표이미지: orders[0].items[0].id.imageUrl,
           상품목록: itemsArr,
           주문번호: orders[i]._id,
           주문날짜: orders[i].createdAt,
@@ -84,14 +84,17 @@ class OrderService {
         };
         result.push(obj);
       }
+      console.log("ORDER : 리스트 ", result);
       return result;
     }
   }
 
   async orderCancel(data) {
     const { id, currentRole } = data;
+    console.log("ORDER : 취소 서비스 들어옴");
     // 토큰 권한이 관리자일때 수정로직
     if (currentRole === "admin") {
+      console.log("ORDER : 관리자 캔슬");
       // 요청받은 주문번호가 수정가능한 상태인지 체크
       const statusCheck = await Order.findById({ _id: id });
       if (statusCheck.orderStatus === "수정가능") {
@@ -103,16 +106,20 @@ class OrderService {
           }
         );
         return;
-      } else {
-        await Order.updateMany(
-          { _id: id },
-          {
-            deliveryStatus: "주문취소",
-            orderStatus: "수정불가",
-          }
-        );
-        return;
       }
+      // 토큰권한 사용자일 경우
+    } else if (currentRole === "user") {
+      console.log("ORDER : 사용자가 주문취소 버튼 누름");
+      await Order.updateOne(
+        { _id: id },
+        {
+          deliveryStatus: "주문취소",
+          orderStatus: "수정불가",
+        }
+      );
+      return;
+    } else {
+      throw new Error("ORDER: 로그인 사용자만 이용 가능한 서비스입니다.");
     }
   }
   // 수정 이유가 배송상태 변경일 경우 함수
--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -27,26 +27,60 @@ const find_order = async () => {
     return;
   }
   for (let i = 0; i < data.length; i++) {
+    // 최상위 div
     const list_son = document.createElement("div");
-    list_son.innerHTML = `
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSThD05wMOmwEeu3Vbe94JDBhWpHBnB--VXXA&usqp=CAU"
-            />
-            <div>
-              <p>총 ${data[i].상품목록.length}개의 상품</p>
-              <span class="total_price">${data[i].총금액} 원 </span><span> | ${list.data[i].주문날짜}</span>
-              <p>요청사항 : ${data[i].요청사항}</p>
-              <span>주문번호 : ${data[i].주문번호}</span>
-              </div>
-              <div>
-              <p>${data[i].배송상태}</p>
-              <button id="${data[i]._id}" onclick="orderCancel(this.id)">주문 취소</button>
-            </div>
-    `;
+    list_son.className = "list_son";
+    // 정보가 담기는 div
+    const list_img = document.createElement("img");
+    list_img.src = data[i].대표이미지;
+    list_img.className = "order_img";
+    const list_div_info = document.createElement("div");
+    list_div_info.className = "list_div_info";
+    const list_div_info_div = document.createElement("div");
+    list_div_info_div.className = "list_div_info_div";
+    const list_items = document.createElement("p");
+    const list_span_totalPrice = document.createElement("span");
+    const list_deliveryMsg = document.createElement("p");
+    const list_orderId = document.createElement("span");
+    list_items.textContent = `${data[i].상품목록[0].상품}외 ${data[i].상품목록.length}개`;
+    list_span_totalPrice.textContent = `결제 금액 : ${data[i].총금액} 원 |  ${list.data[i].주문날짜}`;
+    list_deliveryMsg.textContent = `요청사항 : ${data[i].요청사항}`;
+    list_orderId.textContent = `주문번호 : ${data[i].주문번호}`;
+    list_div_info_div.appendChild(list_items);
+    list_div_info_div.appendChild(list_span_totalPrice);
+    list_div_info_div.appendChild(list_deliveryMsg);
+    list_div_info_div.appendChild(list_orderId);
+    list_div_info.appendChild(list_img);
+    list_div_info.appendChild(list_div_info_div);
+    // 주문상태
+    const list_div_status = document.createElement("div");
+    list_div_status.className = "list_div_status";
+    const list_div_status_p = document.createElement("p");
+    list_div_status_p.textContent = `${data[i].배송상태}`;
+    list_div_status.appendChild(list_div_status_p);
+
+    // 주문수정 가능할시 삭제버튼 추가
+    if (data[i].수정 === "수정가능") {
+      const delBtn = document.createElement("button");
+      delBtn.id = data[i]._id;
+      delBtn.textContent = "주문취소";
+      delBtn.addEventListener("click", async () => {
+        const body = { id: data[i].주문번호, reson: "고객취소" };
+        const a = await Api.patch("/api/orders", "", body);
+        find_order();
+        alert("주문이 취소되었습니다.");
+      });
+      list_div_status.appendChild(delBtn);
+    }
+    list_son.appendChild(list_div_info);
+    list_son.appendChild(list_div_status);
     list_mom.appendChild(list_son);
   }
   console.log("끝");
   // list.data[i];
 };
 
+function orderCancel(id) {
+  console.log(id);
+}
 findOrder.addEventListener("click", find_order);

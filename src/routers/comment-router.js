2363a0446a4df5d07f60fc62b15d733724c37ade
strombreeding <speaker1403@gmail.com>
2022-11-08 01:12:43 +0900

    FEAT: add Api - viewCommentsInfo

--- src/routers/comment-router.js
+++ src/routers/comment-router.js
@@ -5,8 +5,10 @@ const commentRouter = express();
 
 // 리뷰 추가
 commentRouter.post("/", loginRequired, async (req, res, next) => {
+  console.log("리뷰추가 라우터에 오신걸 환영합니다.");
   const { currentUserId } = req;
   const { text, itemId } = req.body;
+  console.log("text , itemId = ", text, itemId);
   // 후에 비속어 배열모음후 추가할 예정
   // if(비속어필터배열.includes(text)){
   //     throw new Error("비속어는 금지에요")
@@ -26,6 +28,25 @@ commentRouter.post("/", loginRequired, async (req, res, next) => {
       status: 201,
       msg: "리뷰가 추가됐습니다.",
       data: success,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//상품 디테일에서 리뷰 보기
+commentRouter.get("/:id", async (req, res, next) => {
+  const itemId = req.params.id;
+  console.log("itemId : ", itemId);
+  if (!itemId) {
+    throw new Error("아이템 id 값을 다시 가져오시게");
+  }
+  try {
+    const result = await commentService.getAll(itemId);
+    return res.status(200).json({
+      status: 200,
+      msg: "해당상품의 댓글들은 이러함",
+      data: result,
     });
   } catch (err) {
     next(err);
--- src/services/comment-service.js
+++ src/services/comment-service.js
@@ -7,9 +7,8 @@ class CommentService {
   async addComment(text, itemId, _id) {
     const user = await User.findById(_id);
     const item = await Item.findById(itemId);
-
     // 리뷰등록 제한
-    if (item.comments.includes(itemId)) {
+    if (user.ownComments.includes(itemId)) {
       throw new Error("리뷰는 한번만 등록할 수 있습니다.");
     }
 
@@ -22,7 +21,33 @@ class CommentService {
     // 해당 아이템DB에 생성된 댓글 id 넘기고 저장
     item.comments.push(newComment._id);
     item.save();
+    // 중복리뷰를 막기위해 유저 리뷰배열에 저장
+    user.ownComments.push(itemId);
+    user.save();
+    console.log(await Item.findById(itemId));
     return newComment;
+  }
+
+  // 상품에 추가된 댓글 보기
+  async getAll(_id) {
+    // item.commets 를 찾음
+    const comments = (await Item.findById(_id).populate("comments")).comments;
+    console.log("포문 돌아감@@");
+
+    //찾은 commets가 user를 참조하기위해 for문 실행
+    // 아래 빈 배열에 리턴할 내용을 추가해줄것임
+    let commentOwners = [];
+    for (let i = 0; i < comments.length; i++) {
+      const commentData = await comments[i].populate("owner");
+      const obj = {
+        _id: commentData.owner._id,
+        name: commentData.owner.name,
+        createdAt: commentData.createdAt,
+        text: commentData.text,
+      };
+      commentOwners.push(obj);
+    }
+    return commentOwners;
   }
 }
 

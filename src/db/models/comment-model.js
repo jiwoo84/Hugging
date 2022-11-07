dca8b2b6ad190cdf1e8ba5f1a27a124b52253e1a
이진희 <speaker1403@gmail.com>
2022-11-07 22:04:02 +0900

    FEAT: add files

---
+++ src/db/models/comment-model.js
@@ -0,0 +1,2 @@
+import { model } from "mongoose";
+import { commentSchema } from "../models/";
---
+++ src/db/schemas/coupon-schema.js
@@ -0,0 +1,11 @@
+import { Schema } from "mongoose";
+
+const couponSchema = new Schema({
+  name: { type: String },
+  discount: Number,
+  owner: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  // expired 해보기
+});
+
+export { couponSchema };

9e1c17d4fc396ac415606ad2b4517180c6dabb7f
Son HyeongSeok <74246053+sonhs1996@users.noreply.github.com>
2022-11-08 22:10:27 +0900

    HOTFIX : FIX coupon schema

--- src/db/schemas/coupon-schema.js
+++ src/db/schemas/coupon-schema.js
@@ -4,8 +4,7 @@ const couponSchema = new Schema({
   name: { type: String },
   discount: Number,
   owner: { type: Schema.Types.ObjectId, ref: "User" },
-  createdAt: { type: Date, default: Date.now, expires: 20 },
-  // expired 해보기
+  createdAt: { type: Date, default: Date.now },
 });
 
 export { couponSchema };

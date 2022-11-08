0bab092ddc2bca3ea39a36bfd5ca8c4f6da7de96
Park Ji Hye <qkrwlgp235@gmail.com>
2022-11-08 18:10:20 +0900

    Feat 회원가입 다음우편번호API 추가

--- src/views/register/findAddress.js
+++ src/views/register/findAddress.js
@@ -1,40 +1,38 @@
 const postalCodeInput = document.querySelector("#postalCodeInput");
-const addressInput = document.querySelector("#addressInput");
-const addAddressInput = document.querySelector("#addAddressInput");
-const findAddressBtn = document.querySelector("#findAddressBtn");
-
-findAddressBtn.addEventListener("click", findAddress);
-
-export function findAddress() {
-  new daum.Postcode({
+const address1Input = document.querySelector("#address1Input");
+const address2Input = document.querySelector("#address2Input");
+function findAddress(){
+    new daum.Postcode({
     oncomplete: function (data) {
-      let addr = "";
-      let extraAddr = "";
+        let addr = "";
+        let extraAddr = "";
 
-      if (data.userSelectedType === "R") {
-        addr = data.roadAddress;
-      } else {
-        addr = data.jibunAddress;
-      }
+        if (data.userSelectedType === "R") {
+            addr = data.roadAddress;
+        } else {
+            addr = data.jibunAddress;
+        }
 
-      if (data.userSelectedType === "R") {
-        if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
-          extraAddr += data.bname;
+        if (data.userSelectedType === "R") {
+            if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+            extraAddr += data.bname;
+            }
+            if (data.buildingName !== '' && data.apartment === "Y") {
+            extraAddr +=
+                extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
+            }
+            if (extraAddr !== "") {
+            extraAddr = " ("  + extraAddr + ")";
+            }
+        } else {
         }
-        if (data.buildingName !== "" && data.apartment === "Y") {
-          extraAddr +=
-            extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
-        }
-        if (extraAddr !== "") {
-          extraAddr = " (" + extraAddr + ")";
-        }
-      } else {
-      }
 
-      postalCodeInput.value = data.zonecode;
-      addressInput.value = `${addr} ${extraAddr}`;
-      addAddressInput.placeholder = "상세 주소를 입력해 주세요.";
-      addAddressInput.focus();
-    },
-  }).open();
+        postalCodeInput.value = data.zonecode;
+        address1Input.value = `${addr} ${extraAddr}`;
+        address2Input.placeholder = "상세 주소를 입력해 주세요.";
+        address2Input.focus();
+        },
+    }).open({ autoClose: true});
 }
+
+export {findAddress};

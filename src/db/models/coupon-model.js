96d550696510313b01985dfb5412a52ae12b5ff4
Son HyeongSeok <74246053+sonhs1996@users.noreply.github.com>
2022-11-10 15:40:26 +0900

    add : coupon model EXPIRE INDEX

--- src/db/models/coupon-model.js
+++ src/db/models/coupon-model.js
@@ -2,5 +2,6 @@ import { model } from "mongoose";
 import { couponSchema } from "../schemas/coupon-schema";
 
 const Coupon = model("Coupon", couponSchema);
+Coupon.schema.index({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 * 24 * 7 });
 
 export { Coupon };

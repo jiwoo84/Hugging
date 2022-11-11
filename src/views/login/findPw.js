5695e13d234c8413061df936560b7b10f763ac41
이진희 <speaker1403@gmail.com>
2022-11-11 15:04:55 +0900

    FEAT: add 비번찾기

---
+++ src/views/login/findPw.js
@@ -0,0 +1,12 @@
+const findPw = document.getElementById("findPw");
+const findwPwForm = document.getElementById("findwPwForm");
+const makeForm = async () => {
+  findwPwForm.innerHTML = `
+        <input placeholder="가입한 이메일을 입력해주세요" id="findPwInput"/>
+    `;
+  const form = document.createElement("form");
+  const authInput = document.createElement("input");
+  authInput.placeholder;
+};
+
+findPw.addEventListener("click", makeForm);
